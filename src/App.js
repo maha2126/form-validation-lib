2ffa324f787fa4cfd2524e39ed946f9f3ef9985c
maha2126 <mariam.petrosyan1998@mail.ru>
2023-03-08 01:18:18 +0400

    form-alidation-lib

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {useForm} from 'react-hook-form'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const {register, handleSubmit, formState:{errors}} = useForm();
+
+  const addUser = data =>{
+    console.log(data)
+  }
+  return <div>
+    <h1>Signup</h1>
+    <form className='form' onSubmit={handleSubmit(addUser)} >
+      {errors.first && errors.last && <p>First or Last name is required...</p>}
+      {errors.age && <p>Age must be a number between 18 and 30</p>}
+      <div>
+        <label>First name</label>
+        <input type="text" {...register("first",{required:true})}/>
+      </div>
+      <div>
+      <label>Last name</label>
+        <input type="text" {...register("...last",{required:true})}/>
+      </div>
+      <div>
+        <label>Age</label>
+        <input type="number" {...register("...age",{pattern:/[\d]/,required:true, min:18, max:30})}/>
+      </div>
+      <button>Save</button>
+    </form>
+  </div>
 }
-
 export default App;
